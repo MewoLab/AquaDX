660316f8d50102ecbfef0456b93e5d8b26083bd3
Menci <huanghaorui301@gmail.com>
2024-11-07 16:40:04 +0800

    Cheat

--- AquaMai/Mods/Cheat/TicketUnlock.cs
+++ AquaMai/Mods/Cheat/TicketUnlock.cs
@@ -1,40 +1,43 @@
 using Manager.MaiStudio;
 using HarmonyLib;
+using AquaMai.Attributes;
 
-namespace AquaMai.Mods.Cheat
+namespace AquaMai.Mods.Cheat;
+
+/**
+ * Unlock tickets that are typically locked unless a specific event is open.
+ */
+[ConfigSection(
+    en: "Unlock normally event-only tickets",
+    zh: "解锁游戏里所有可能的跑图券")]
+public class TicketUnlock
 {
-    /**
-     * Unlock tickets that are typically locked unless a specific event is open.
-     */
-    public class TicketUnlock
+    // For any ticket, return the event ID 1 to unlock it
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(TicketData), "get_ticketEvent")]
+    public static bool get_ticketEvent(ref StringID __result)
     {
-        // For any ticket, return the event ID 1 to unlock it
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(TicketData), "get_ticketEvent")]
-        public static bool get_ticketEvent(ref StringID __result)
+        var id = new Manager.MaiStudio.Serialize.StringID
         {
-            var id = new Manager.MaiStudio.Serialize.StringID
-            {
-                id = 1,
-                str = "無期限常時解放"
-            };
-            
-            var sid = new StringID();
-            sid.Init(id);
-            
-            __result = sid;
-            return false;
-        }
-        
-        // Modify the maxTicketNum to 0
-        // this is because TicketManager.GetTicketData adds the ticket to the list if either
-        // the player owns at least one ticket or the maxTicketNum = 0
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(TicketData), "get_maxCount")]
-        public static bool get_maxCount(ref int __result)
-        {
-            __result = 0;
-            return false;
-        }
+            id = 1,
+            str = "無期限常時解放"
+        };
+
+        var sid = new StringID();
+        sid.Init(id);
+
+        __result = sid;
+        return false;
+    }
+
+    // Modify the maxTicketNum to 0
+    // this is because TicketManager.GetTicketData adds the ticket to the list if either
+    // the player owns at least one ticket or the maxTicketNum = 0
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(TicketData), "get_maxCount")]
+    public static bool get_maxCount(ref int __result)
+    {
+        __result = 0;
+        return false;
     }
 }
--- AquaMai/Mods/Cheat/UnlockUtage.cs
+++ AquaMai/Mods/Cheat/UnlockUtage.cs
@@ -3,18 +3,20 @@ using HarmonyLib;
 using MAI2System;
 using Manager;
 
-namespace AquaMai.Mods.Cheat
+namespace AquaMai.Mods.Cheat;
+
+[ConfigSection(
+    en: "Unlock Utage without the need of DXRating 10000",
+    zh: "不需要万分也可以进宴会场")]
+[GameVersion(24000)]
+public class UnlockUtage
 {
-    [GameVersion(24000)]
-    public class UnlockUtage
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(GameManager), "CanUnlockUtageTotalJudgement")]
+    public static bool CanUnlockUtageTotalJudgement(out ConstParameter.ResultOfUnlockUtageJudgement result1P, out ConstParameter.ResultOfUnlockUtageJudgement result2P)
     {
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(GameManager), "CanUnlockUtageTotalJudgement")]
-        public static bool CanUnlockUtageTotalJudgement(out ConstParameter.ResultOfUnlockUtageJudgement result1P, out ConstParameter.ResultOfUnlockUtageJudgement result2P)
-        {
-            result1P = ConstParameter.ResultOfUnlockUtageJudgement.Unlocked;
-            result2P = ConstParameter.ResultOfUnlockUtageJudgement.Unlocked;
-            return false;
-        }
+        result1P = ConstParameter.ResultOfUnlockUtageJudgement.Unlocked;
+        result2P = ConstParameter.ResultOfUnlockUtageJudgement.Unlocked;
+        return false;
     }
 }
