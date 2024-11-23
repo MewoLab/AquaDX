0af22d61b50c910a762cb4133a39a6a21398a410
Menci <huanghaorui301@gmail.com>
2024-11-23 14:57:25 +0800

    update

--- AquaMai/AquaMai/BuildInfo.cs
+++ AquaMai/AquaMai/BuildInfo.cs
@@ -4,7 +4,7 @@ public static class BuildInfo
 {
     public const string Name = "AquaMai";
     public const string Description = "Mod for Sinmai";
-    public const string Author = "Aza";
+    public const string Author = "Aza ft. Clansty ft. Menci";
     public const string Company = null;
     public const string Version = "1.2.2";
     public const string DownloadLink = null;
