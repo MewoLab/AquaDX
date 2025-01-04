d7b7d617bdb1df9a46f6c9c383250ee7580d015f
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-01-04 08:48:56 -0500

    [F] Fix user activity saving

--- src/main/java/icu/samnyan/aqua/sega/chusan/model/userdata/UserActivity.kt
+++ src/main/java/icu/samnyan/aqua/sega/chusan/model/userdata/UserActivity.kt
@@ -8,9 +8,7 @@ import jakarta.persistence.UniqueConstraint
 
 @Entity(name = "ChusanUserActivity")
 @Table(name = "chusan_user_activity", uniqueConstraints = [UniqueConstraint(columnNames = ["user_id", "kind", "activity_id"])])
-class UserActivity(user: Chu3UserData) : Chu3UserEntity() {
-    init { this.user = user }
-
+class UserActivity : Chu3UserEntity() {
     var kind = 0
     @JsonProperty("id")
     @Column(name = "activity_id")
