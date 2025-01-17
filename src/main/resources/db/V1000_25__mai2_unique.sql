f43eaa45770661e390112760ac80e7dea53af00a
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-01-17 09:32:31 -0500

    [F] Fix key conflicts

--- src/main/resources/db/V1000_25__mai2_unique.sql
+++ src/main/resources/db/V1000_25__mai2_unique.sql
@@ -92,40 +92,40 @@ WHERE id NOT IN (
 
 # Add unique constraint
 ALTER TABLE maimai2_user_extend
-    ADD CONSTRAINT unique_user_extend UNIQUE (user_id);
+    ADD CONSTRAINT unique_maimai2_user_extend UNIQUE (user_id);
 
 ALTER TABLE maimai2_user_option
-    ADD CONSTRAINT unique_user_option UNIQUE (user_id);
+    ADD CONSTRAINT unique_maimai2_user_option UNIQUE (user_id);
 
 ALTER TABLE maimai2_user_character
-    ADD CONSTRAINT unique_user_character UNIQUE (user_id, character_id);
+    ADD CONSTRAINT unique_maimai2_user_character UNIQUE (user_id, character_id);
 
 ALTER TABLE maimai2_user_map
-    ADD CONSTRAINT unique_user_map UNIQUE (user_id, map_id);
+    ADD CONSTRAINT unique_maimai2_user_map UNIQUE (user_id, map_id);
 
 ALTER TABLE maimai2_user_login_bonus
-    ADD CONSTRAINT unique_user_login_bonus UNIQUE (user_id, bonus_id);
+    ADD CONSTRAINT unique_maimai2_user_login_bonus UNIQUE (user_id, bonus_id);
 
 ALTER TABLE maimai2_user_udemae
-    ADD CONSTRAINT unique_user_udemae UNIQUE (user_id);
+    ADD CONSTRAINT unique_maimai2_user_udemae UNIQUE (user_id);
 
 ALTER TABLE maimai2_user_general_data
-    ADD CONSTRAINT unique_user_general_data UNIQUE (user_id, property_key);
+    ADD CONSTRAINT unique_maimai2_user_general_data UNIQUE (user_id, property_key);
 
 ALTER TABLE maimai2_user_item
-    ADD CONSTRAINT unique_user_item UNIQUE (user_id, item_id, item_kind);
+    ADD CONSTRAINT unique_maimai2_user_item UNIQUE (user_id, item_id, item_kind);
 
 ALTER TABLE maimai2_user_music_detail
-    ADD CONSTRAINT unique_user_music_detail UNIQUE (user_id, music_id, level);
+    ADD CONSTRAINT unique_maimai2_user_music_detail UNIQUE (user_id, music_id, level);
 
 ALTER TABLE maimai2_user_course
-    ADD CONSTRAINT unique_user_course UNIQUE (user_id, course_id);
+    ADD CONSTRAINT unique_maimai2_user_course UNIQUE (user_id, course_id);
 
 ALTER TABLE maimai2_user_friend_season_ranking
-    ADD CONSTRAINT unique_user_friend_season_ranking UNIQUE (user_id, season_id);
+    ADD CONSTRAINT unique_maimai2_user_friend_season_ranking UNIQUE (user_id, season_id);
 
 ALTER TABLE maimai2_user_favorite
-    ADD CONSTRAINT unique_user_favorite UNIQUE (user_id, item_kind);
+    ADD CONSTRAINT unique_maimai2_user_favorite UNIQUE (user_id, item_kind);
 
 ALTER TABLE maimai2_user_activity
-    ADD CONSTRAINT unique_user_activity UNIQUE (user_id, activity_id, kind);
+    ADD CONSTRAINT unique_maimai2_user_activity UNIQUE (user_id, activity_id, kind);
