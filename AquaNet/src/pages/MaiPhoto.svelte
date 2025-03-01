150b9915ad5014e053908e8855610581ef6cfe93
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-03-01 01:25:29 -0500

    [S] Better styling

--- AquaNet/src/pages/MaiPhoto.svelte
+++ AquaNet/src/pages/MaiPhoto.svelte
@@ -1,31 +1,38 @@
 ﻿<script lang="ts">
   import {GAME} from "../libs/sdk";
   import {AQUA_HOST} from "../libs/config";
-
-  let photos: string[] = []
-  GAME.photos().then(result => {
-    console.log(result)
-    photos = result
-  }).catch(e => {
-    console.error(e)
-  })
+  import Loading from "../components/ui/Loading.svelte";
+  import Error from "../components/ui/Error.svelte";
+  import { t } from "../libs/i18n";
 
   const token = localStorage.getItem("token")
 </script>
 
-<main>
-  <div class="content-main">
-    <p>here you can see the pictures you have uploaded from mai.</p>
-  <div class="pictures">
-  {#each photos as photo}
-    <img class="rounded-2xl max-w-sm" src="{AQUA_HOST}/api/v2/game/mai2/my-photo/{photo}?token={token}" alt="Memorial" />
-  {/each}
-  {#if photos.length === 0}
-    <p>There are no photos available. Try uploading some!</p>
-  {/if}
+<main class="content">
+  <div class="outer-title-options">
+    <h2>{t("maiphoto.title")}</h2>
   </div>
+
+  {#await GAME.photos()}
+    <Loading/>
+  {:then photos}
+    {#if photos.length === 0}
+      <blockquote>{t('maiphoto.none')}</blockquote>
+    {:else}
+      <blockquote>{t('maiphoto.url_warning')}</blockquote>
+    {/if}
+    <div class="pictures">
+      {#each photos as photo}
+        <div class="photo-container">
+          <img class="rounded-2xl" src="{AQUA_HOST}/api/v2/game/mai2/my-photo/{photo}?token={token}" alt="Memorial" />
+        </div>
+      {/each}
     </div>
+  {:catch error}
+    <Error {error}/>
+  {/await}
 </main>
+
 <style lang="sass">
   @use "../vars"
 
@@ -35,4 +42,16 @@
     justify-content: center
     row-gap: 1rem
     gap: 1rem
+
+  .photo-container
+    flex: 1 1 300px
+    min-width: 280px
+    max-width: 100%
+    display: flex
+    justify-content: center
+
+  .photo-container img
+    width: 100%
+    height: auto
+    object-fit: contain
 </style>
