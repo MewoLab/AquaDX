ccd63678751ad60bf77d59631cf75fee7ab29d20
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-10-25 05:12:15 +0800

    [+] Firendly note

--- src/main/resources/db/80/V1000_60__drop_the_codebase.sql
+++ src/main/resources/db/80/V1000_60__drop_the_codebase.sql
@@ -1,4 +1,4 @@
-# assuming you all have backups :3
+# there are 0 profiles here so should be fine.
 drop table maimai_user_web_option;
 drop table maimai_user_survival;
 drop table maimai_user_present_event;
@@ -12,6 +12,7 @@ drop table maimai_user_activity;
 
 drop table maimai_user_data;
 
+# there are 30 profiles in chuni, so make sure to back up before upgrading!
 drop table chuni_game_character;
 drop table chuni_game_charge;
 drop table chuni_game_event;
