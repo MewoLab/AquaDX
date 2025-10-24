1ca1e3edc3fd98058b2449bdd11e470ea86b4ab2
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-10-25 04:26:06 +0800

    [-] Drop maimai finale database

---
+++ src/main/resources/db/80/V1000_60__drop_the_codebase.sql
@@ -0,0 +1,13 @@
+# assuming you all have backups :3
+drop table main.maimai_user_web_option;
+drop table main.maimai_user_survival;
+drop table main.maimai_user_present_event;
+drop table main.maimai_user_playlog;
+drop table main.maimai_user_option;
+drop table main.maimai_user_music_detail;
+drop table main.maimai_user_general_data;
+drop table main.maimai_user_character;
+drop table main.maimai_user_boss;
+drop table main.maimai_user_activity;
+
+drop table main.maimai_user_data;
