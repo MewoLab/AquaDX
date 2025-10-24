6d99beafe930839195b69aba809968f555137289
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-10-25 04:35:57 +0800

    [F]

--- src/main/resources/db/80/V1000_60__drop_the_codebase.sql
+++ src/main/resources/db/80/V1000_60__drop_the_codebase.sql
@@ -1,13 +1,15 @@
 # assuming you all have backups :3
-drop table main.maimai_user_web_option;
-drop table main.maimai_user_survival;
-drop table main.maimai_user_present_event;
-drop table main.maimai_user_playlog;
-drop table main.maimai_user_option;
-drop table main.maimai_user_music_detail;
-drop table main.maimai_user_general_data;
-drop table main.maimai_user_character;
-drop table main.maimai_user_boss;
-drop table main.maimai_user_activity;
+drop table maimai_user_web_option;
+drop table maimai_user_survival;
+drop table maimai_user_present_event;
+drop table maimai_user_playlog;
+drop table maimai_user_option;
+drop table maimai_user_music_detail;
+drop table maimai_user_general_data;
+drop table maimai_user_character;
+drop table maimai_user_boss;
+drop table maimai_user_activity;
 
-drop table main.maimai_user_data;
+drop table maimai_user_data;
+
+
