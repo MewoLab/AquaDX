fa068b71728c060dab8f8cddeecb4650f3dc86cf
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2026-03-28 04:59:29 -0400

    [+] Drop tables

---
+++ src/main/resources/db/80/V1000_71__drop_game_data_tables.sql
@@ -0,0 +1,24 @@
+-- Drop game data tables that have been migrated to JSON
+DROP TABLE IF EXISTS chusan_game_charge;
+DROP TABLE IF EXISTS chusan_game_event;
+DROP TABLE IF EXISTS chusan_game_gacha;
+DROP TABLE IF EXISTS chusan_game_gacha_card;
+DROP TABLE IF EXISTS chusan_game_login_bonus;
+DROP TABLE IF EXISTS chusan_game_login_bonus_preset;
+DROP TABLE IF EXISTS chusan_game_linked_verse;
+
+DROP TABLE IF EXISTS maimai2_game_charge;
+DROP TABLE IF EXISTS maimai2_game_event;
+DROP TABLE IF EXISTS maimai2_game_selling_card;
+DROP TABLE IF EXISTS maimai2_game_ticket;
+
+DROP TABLE IF EXISTS ongeki_game_card;
+DROP TABLE IF EXISTS ongeki_game_chara;
+DROP TABLE IF EXISTS ongeki_game_event;
+DROP TABLE IF EXISTS ongeki_game_gacha;
+DROP TABLE IF EXISTS ongeki_game_gacha_card;
+DROP TABLE IF EXISTS ongeki_game_music;
+DROP TABLE IF EXISTS ongeki_game_point;
+DROP TABLE IF EXISTS ongeki_game_present;
+DROP TABLE IF EXISTS ongeki_game_reward;
+DROP TABLE IF EXISTS ongeki_game_skill;
