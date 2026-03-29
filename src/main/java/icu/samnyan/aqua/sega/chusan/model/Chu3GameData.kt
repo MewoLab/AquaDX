9979046c193dbb8dcbc16039c0be7b674df79f41
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2026-03-29 16:43:14 -0400

    [-] remove enabled field

--- src/main/java/icu/samnyan/aqua/sega/chusan/model/Chu3GameData.kt
+++ src/main/java/icu/samnyan/aqua/sega/chusan/model/Chu3GameData.kt
@@ -20,7 +20,6 @@ class GameEvent {
     val type = 0
     val startDate: LocalDateTime? = LocalDateTime.of(2019, 1, 1, 0, 0, 0)
     val endDate: LocalDateTime? = LocalDateTime.of(2099, 1, 1, 0, 0, 0)
-    val enable = false
 }
 
 class GameGacha {
