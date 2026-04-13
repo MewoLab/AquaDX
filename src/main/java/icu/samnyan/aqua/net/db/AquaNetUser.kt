28f23f368052995a759fd0579b69b6d9c9bb3a23
Raymond <101374892+raymonable@users.noreply.github.com>
2026-04-12 22:48:23 -0400

    fix: automatically generate keychip on user creation

--- src/main/java/icu/samnyan/aqua/net/db/AquaNetUser.kt
+++ src/main/java/icu/samnyan/aqua/net/db/AquaNetUser.kt
@@ -147,9 +147,18 @@ class AquaUserServices(
             }
     }
 
+    val keychipRange = 1e9.toULong()..1e10.toULong() - 1UL
+    private fun generateKeychipId(): String {
+        // 1337 is retained for backwards compatibility, it's no longer required for cabinet keychips
+        var keychip = "A" + keychipRange.random() + "1337"
+        while ( userKeychipRepo.existsByKeychipId(keychip) )
+            keychip = "A" + keychipRange.random() + "1337"
+        return keychip
+    }
+
     fun create(username: Str, email: Str, password: Str, country: Str, emailConfirmed: Boolean = false): AquaNetUser {
         // Create user
-        val u = AquaNetUser(
+        val user = AquaNetUser(
             username = checkUsername(username),
             email = validateEmail(email),
             pwHash = checkPwHash(password),
@@ -163,16 +172,20 @@ class AquaUserServices(
             luid = extId.toString()
             registerTime = LocalDateTime.now()
             accessTime = registerTime
-            aquaUser = u
+            aquaUser = user
             isGhost = true
         }
-        u.ghostCard = card
+        user.ghostCard = card
+
+        // Create an automatic keychip
+        val keychip = UserKeychip(0, user, generateKeychipId())
 
         // Save the user
-        userRepo.save(u)
+        userRepo.save(user)
         cardRepo.save(card)
+        userKeychipRepo.save(keychip)
 
-        return u
+        return user
     }
 
     fun update(user: AquaNetUser, key: Str, value: Str) {
